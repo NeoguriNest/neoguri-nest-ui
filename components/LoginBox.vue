046dd6668341c0572841e9e1a8e528a820583573
parkjg20 <parkjg20@naver.com>
2022-09-15 02:19:56 +0900

    버튼 텍스트 수정 및 여백 수정 #2

--- components/LoginBox.vue
+++ components/LoginBox.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="d-flex flex-column px-3">
+  <div class="d-flex flex-column">
     <div class="input-group flex-column mb-3">
       <input type="text" placeholder="아이디" class="py-3 px-4 mb-2" />
       <input type="text" placeholder="비밀번호" class="py-3 px-4" />
     </div>
-    <div class="d-flex">
-      <label class="d-flex align-items-center">
+    <div class="d-flex mb-4">
+      <label class="d-flex align-items-center mb-0">
         <input type="checkbox" name="maintain_login" />
         <p class="ml-2 mb-0">로그인 상태 유지</p>
       </label>
     </div>
     <div class="login-buttons d-flex flex-column">
-      <button class="neoguri-btn-style-1 py-3 border-0">로그인</button>
+      <button class="neoguri-btn-style-1 py-3 border-0">너구리소굴 로그인</button>
     </div>
   </div>
 </template>
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="d-flex flex-column">
-    <LoginBox />
+    <HeaderBox title="로그인" />
+
+    <div class="d-flex flex-column px-3">
+      <div class="d-flex w-100 py-4">
+        <img class="w-100 h-100" src="/img/intro-banner.png" alt="반갑다구리 배너"/>
+
+      </div>
+
+
+      <LoginBox />
+    </div>
   </div>
 
 </template>
