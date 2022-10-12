5f5c69a466927c80ff7844f7ee81a2b6c9c480c9
parkjg20 <parkjg20@naver.com>
2022-10-12 22:31:44 +0900

    회원가입 depth2: 프로필 정보 입력 화면 구현중, select 인풋 타입 작업중 #1 #4

---
+++ components/common/inputs/AbstractSelect.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="d-flex flex-column mb-3">
+    <div class="input-group flex-column mb-1">
+      <div class="mb-2 d-flex">
+        <span class="neoguri-font-weight-500 mb-0">{{ this.title }}</span>
+        <span v-if="this.required" class="mb-0 ml-1 color-neoguri-red">*</span>
+      </div>
+      <select v-model="value" class="py-3 px-4">
+      </select>
+    </div>
+    <div class="d-flex">
+      <span v-if="this.value.length && this.validate()" class="color-neoguri-valid">
+        {{ this.validDescription }}
+      </span>
+      <span v-if="this.value.length && !this.validate()" class="color-neoguri-invalid">
+        {{ this.invalidDescription }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend(
+  {
+  name: 'AbstractSelect',
+  props: {
+    name: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: 'text'
+    },
+    required: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      required: false
+    },
+    validDescription: {
+      type: String,
+      required: false
+    },
+    invalidDescription: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+    computedPlaceholder() {
+      return (this && this.placeholder)
+        ? this.placeholder
+        : this.title
+    },
+  },
+  methods: {
+    validate(): boolean {
+      // TODO: override
+      throw new Error('Method must be implemented. name: \'validate\'')
+    }
+  },
+  watch: {
+    value(newValue: string, oldValue: string) {
+      this.$emit('input', { value: newValue, isValid: this.validate() } )
+    }
+  }
+})
+</script>
+
+<style scoped>
+input {
+  border: 1px solid #DFDFDF;
+}
+
+input::placeholder {
+  color: #949494;
+}
+
+</style>
--- pages/Register.vue
+++ pages/Register.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="d-flex flex-column">
-    <HeaderBox title="너구리단 가입" />
+    <template>
 
-    <div class="neoguri-container d-flex flex-column px-3 container-sm pb-5">
+      <HeaderBox title="너구리단 가입" />
 
-      <div class="pt-2">
-        <LoginIdInput name="login_id" title="아이디" v-model="loginId" confirm-button-title="중복확인" required />
-      </div>
+      <div class="neoguri-container d-flex flex-column px-3 container-sm pb-5">
 
-      <div class="pt-2">
-        <PasswordInput name="password" title="비밀번호" v-model="password" required />
-      </div>
+        <div class="pt-2">
+          <LoginIdInput name="login_id" title="아이디" v-model="loginId" confirm-button-title="중복확인" required />
+        </div>
 
-      <div class="pt-2">
-        <PasswordConfirmInput name="password_confirm" title="비밀번호 확인" required v-model="passwordConfirm" :password="password.value" />
-      </div>
+        <div class="pt-2">
+          <PasswordInput name="password" title="비밀번호" v-model="password" required />
+        </div>
 
-      <div class="pt-2">
-        <EmailInput name="email" title="이메일" confirm-button-title="중복확인" v-model="email" required />
-      </div>
+        <div class="pt-2">
+          <PasswordConfirmInput name="password_confirm" title="비밀번호 확인" required v-model="passwordConfirm" :password="password.value" />
+        </div>
 
-      <div v-if="this.isInvalid" class="pt-3">
-        <template v-for="description in this.invalidInputDescriptions">
-          <p class="pt-2">{{ description }}</p>
-        </template>
+        <div class="pt-2">
+          <EmailInput name="email" title="이메일" confirm-button-title="중복확인" v-model="email" required />
+        </div>
+
+        <div v-if="this.isInvalid" class="pt-3">
+          <template v-for="description in this.invalidInputDescriptions">
+            <p class="pt-2">{{ description }}</p>
+          </template>
+        </div>
+        <div class="d-flex pt-3 flex-column">
+          <button class="neoguri-btn-style-1 py-3 border-0" @click="submit">너구리단 가입하기</button>
+        </div>
       </div>
-      <div class="d-flex pt-3 flex-column">
-        <button class="neoguri-btn-style-1 py-3 border-0" @click="submit">너구리단 가입하기</button>
+    </template>
+    <template>
+      <HeaderBox title="너구리 등록증 발급" />
+
+      <div class="neoguri-container d-flex flex-column px-3 container-sm pb-5">
+
+        <div class="pt-2">
+          <LoginIdInput name="nickname" title="닉네임" v-model="nickname" confirm-button-title="중복확인" required />
+        </div>
+
+        <div v-if="this.isInvalid" class="pt-3">
+          <template v-for="description in this.invalidInputDescriptions">
+            <p class="pt-2">{{ description }}</p>
+          </template>
+        </div>
+        <div class="d-flex pt-3 flex-column">
+          <button class="neoguri-btn-style-1 py-3 border-0" :disabled="this.isInvalid ? 'true' : null" @click="submit">너구리단 가입하기</button>
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -55,6 +77,9 @@ export default Vue.extend({
       password: { value: '', isValid: false },
       passwordConfirm: { value: '', isValid: false },
       email: { value: '', isValid: false },
+
+      nickname: { value: '', isValid: false },
+
       loginIdDuplicateCheck: false,
       emailDuplicateCheck: false,
       serviceAgreementCheck: false,
@@ -64,6 +89,10 @@ export default Vue.extend({
   computed: {
     isInvalid(): boolean {
       return this.$data.invalidInputDescriptions.length > 0
+        || !this.$data.loginId.isValid
+        || !this.$data.password.isValid
+        || !this.$data.passwordConfirm.isValid
+        || !this.$data.email.isValid
     }
   },
   watch: {
@@ -71,23 +100,8 @@ export default Vue.extend({
   methods: {
     submit() {
       const loginId = this.$data.loginId;
-      if (!loginId.isValid) {
-        return;
-      }
-
       const password = this.$data.password;
-      if (!password.isValid) {
-        return;
-      }
-
-      if (this.$data.passwordConfirm.isValid) {
-        return;
-      }
-
       const email = this.$data.email;
-      if (!email.isValid) {
-        return;
-      }
 
       const payload = {
         loginId: loginId.value,
