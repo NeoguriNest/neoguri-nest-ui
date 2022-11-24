4a1c97f1ece18f05f8317e86e1c88dc10194d98f
parkjg20 <parkjg20@naver.com>
2022-11-25 01:16:55 +0900

    너구리 기본 컴포넌트 추가(버튼, 텍스트 인풋) #7

---
+++ components/common/buttons/NeoguriButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button type="button" :class="((type) ? types[type] + ' ' : '') + 'py-3 border-0'">
+    <slot name="content"></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend(
+  {
+    name: 'NeoguriButton',
+    props: {
+      type: {
+        type: String,
+        required: true
+      },
+      types: {
+        type: Object,
+        default: () => {
+          return {
+            'primary': 'neoguri-btn-style-1',
+            'secondary': 'neoguri-btn-style-2'
+          };
+        },
+      }
+    },
+    data () {
+      return {
+        value: '',
+        isConfirmed: false
+      }
+    },
+    methods: {
+    },
+    watch: {
+      value(value: string) {
+        this.$emit('input', value)
+      }
+    }
+
+  })
+</script>
+
+<style scoped>
+
+  input {
+   border: 1px solid #DFDFDF;
+  }
+
+  input::placeholder {
+    color: #949494;
+  }
+
+</style>
---
+++ components/common/inputs/NeoguriInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="d-flex flex-column mb-3">
+    <div class="input-group flex-column mb-1">
+      <div class="mb-2 d-flex">
+        <span class="neoguri-font-weight-500 mb-0">
+          <slot name="title"></slot>
+        </span>
+      </div>
+      <input :type="this.type" :name="name" :placeholder="this.placeholder" v-model="value" class="py-3 px-4"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend(
+  {
+    name: 'NeoguriInput',
+    props: {
+      type: {
+        type: String,
+        default: 'text'
+      },
+      name: {
+        type: String,
+      },
+      placeholder: {
+        type: String,
+        default: '내용을 입력해주세요'
+      },
+    },
+    data() {
+      return {
+        value: '',
+        isConfirmed: false
+      }
+    },
+    methods: {
+    },
+    watch: {
+      value(value: string) {
+        this.$emit('input', value)
+      }
+    }
+
+  })
+</script>
+
+<style scoped>
+
+  input {
+   border: 1px solid #DFDFDF;
+  }
+
+  input::placeholder {
+    color: #949494;
+  }
+
+</style>
