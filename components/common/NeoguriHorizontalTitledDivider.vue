5eba7c62fcc517ff9bc2ed9a349f2c5a76b38c2b
parkjg20 <parkjg20@naver.com>
2022-11-28 00:23:40 +0900

    기본 컴포넌트 작성중 #7

---
+++ components/common/NeoguriHorizontalTitledDivider.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex flex-row justify-content-center align-items-center position-relative">
+    <div class="divider-background-line d-flex w-100 position-absolute"></div>
+    <div class="d-flex text-center bg-white">
+      <slot name="title"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NeoguriHorizontalTitledDivider"
+}
+</script>
+
+<style scoped>
+  .divider-background-line {
+    border-bottom: 1px solid #DFDFDF;
+    z-index: -1;
+  }
+
+</style>
---
+++ components/common/inputs/radios/NeoguriRadioGroup.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="d-flex flex-column mb-3">
+    <div class="input-group flex-column mb-1">
+      <div class="mb-2 d-flex">
+        <span class="neoguri-font-weight-500 mb-0">
+          <slot name="title"></slot>
+        </span>
+      </div>
+
+      <div class="flex-column">
+        <label v-for="item in this.items">
+          <input type="radio" :name="this.name" :value="item.value" v-model="value">
+          <p>{{ item.text }}</p>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend(
+  {
+  name: 'NeoguriRadioGroup',
+  props: {
+    name: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    /**
+     * @requires items: [{value: String, title: String}, ...]
+     */
+    items: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+    value(value: string) {
+      this.$emit('input', value)
+    }
+  }
+})
+</script>
+
+<style scoped>
+select {
+  border: 1px solid #DFDFDF;
+}
+
+select::placeholder {
+  color: #949494;
+}
+
+</style>
