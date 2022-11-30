7825a9de384d28228ab271547faad302b24dca74
parkjg20 <parkjg20@naver.com>
2022-12-01 00:35:45 +0900

    약관 체크박스 공통 컴포넌트 추가 #7

---
+++ components/common/inputs/checkboxes/NeoguriAgreementCheckbox.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex flex-column">
+    <div class="input-group flex-column mb-1">
+      <div class="d-flex flex-row align-items-center">
+        <label class="d-flex flex-row align-items-center mb-0">
+          <input type="checkbox" :name="this.name" v-model="value">
+          <p class="my-0 mx-0 ml-2 align-middle">{{ title }}</p>
+        </label>
+
+        <button
+          class="ml-3 py-1 px-3 border-0 rounded-sm" @click="callback">
+          약관보기
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend(
+  {
+  name: 'NeoguriAgreementCheckbox',
+  props: {
+    name: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+
+    callback: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+    value(value: string) {
+      this.$emit('input', value)
+    }
+  }
+})
+</script>
+
+<style scoped>
+select {
+  border: 1px solid #DFDFDF;
+}
+
+select::placeholder {
+  color: #949494;
+}
+
+</style>
